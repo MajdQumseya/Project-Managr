1256cb43dcb50713a286cf6ab9911f77d92658c4
MajdQumseya <qumseya.majd@gmail.com>
2023-01-30 16:36:14 +0200

    Added client mutations graphql

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ connectDB()
 
 app.use('/graphql', graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === 'development' 
+    graphiql: process.env.NODE_ENV == 'development' ? 'true' : 'false'
 }))
 
 app.listen(port, () => {
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,7 +2,7 @@ const { projects, clients } = require('../sampleData.js');
 //Mongoose models
 const Project = require('../models/Project');
 const Client = require('../models/Client')
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList } = require('graphql');
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList, GraphQLNonNull } = require('graphql');
 
 //Client Type
 const ClientType = new GraphQLObjectType({
@@ -39,7 +39,7 @@ const RootQuery = new GraphQLObjectType({
             type: new GraphQLList(ClientType),
             resolve(parent, args) {
                 return Client.find()
-            }   
+            }
         },
         client: {
             type: ClientType,
@@ -65,6 +65,41 @@ const RootQuery = new GraphQLObjectType({
     }
 });
 
+//Mutations 
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addClient: {
+            type: ClientType,
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+                email: { type: GraphQLNonNull(GraphQLString) },
+                phone: { type: GraphQLNonNull(GraphQLString) },
+            },
+            resolve(parent, args) {
+                const client = new Client({
+                    name: args.name,
+                    email: args.email,
+                    phone: args.phone
+                })
+
+                return client.save();
+                
+            }
+        },
+        deleteClient: {
+            type:ClientType,
+            args: {
+                id: { type: GraphQLNonNull(GraphQLString) },
+            },
+            resolve(parent,args){
+                return Client.findByIdAndDelete(args.id)
+            }
+        }
+    }
+})
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation
 });
